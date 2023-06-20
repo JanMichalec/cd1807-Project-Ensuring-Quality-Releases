6775a5fec4157161a09e31ad17e957480d1f6467
Jan Michalec <jan.michalec@eon.com>
2023-06-20 10:47:47 +0200

    vars

--- terraform/environments/test/terraform.tfvars
+++ terraform/environments/test/terraform.tfvars
@@ -1,8 +1,8 @@
 # Azure subscription vars
-subscription_id = "d63f8116-7a20-4117-80f3-6b57296b37a5"
+subscription_id = "83316a40-5757-40fd-8e78-645eefac51b6"
 tenant_id = "f958e84a-92b8-439f-a62d-4f45996b6d07"
-client_id = "9f3d8a3b-a457-4c50-bf50-bcdeb33c998b"
-client_secret = "UgF8Q~a4WLrp7cSY2TIZHbaG-8lbvNUfHb~MCcqS"
+client_id = "a8b2e5ba-4637-4711-acea-972b2cc930ea"
+client_secret = "mhG8Q~YBT3v_rlV.0_g8YnVQVWWGGYS422~DOcbX"
 
 # Resource Group/Location
 location = "West Europe"
--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -20,7 +20,7 @@ resource "azurerm_linux_virtual_machine" "" {
   network_interface_ids = []
   admin_ssh_key {
     username   = "honza"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDYCzjiCnf6rLv8uZ14ZeZI3naY3DOUheu4AQaOK+QT35zA5cCUxZr/hqTnWljmCFdNpEPY2vp06VIws6RwYRGQpzTX2JTHuucvK9CKHmnyNKUM2sZVRMLoSxuanxtLu+wIHKjVjY/CfxFsAIwjY57gTr09AJpEaNNFASImh1N+EgXzzNWFPY1LmT6I4+dToUUgthXBYYTmEB/Yt1PN+K/b1mkisiRJo96snHlEmXoAyGU1aLka/Y4nijIKQZWIrV/goHQWlEdF/EIDlUC/dJY0NgMNTeAMpDVp4lvS66loy7T/L0UhJAw+bgQZjQt8pMyzFAFRNBO4Wd8uDvjMsVVxrM5xCRvvywXivL56XlDwoWOLJz/FPiCcTnmwxQgjZ8id2s2Mc8wIP5ZfF3iyetysF+J4cEygzvpq/8FUrVb6W2g07Qa7/UWD2jS5eSqFPQHOgO91DTt18eGwaeTCjIzGZ5jqP4AEVhK29jm9jQY114g91lrzHgiopotYufxFn00= odl_user@cc-f958-1250f49a-7b64c74f8d-qvtnm"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDlCK3043oxQ4fWkE+IHb+AcMYTNw7jHxOW9FP36/29jOMw83hxqVh89j60R5gKcOD1rImdUgIfI/6UoKcSPiN9SS0o8HA+AILHXNAxN/U2sNMpLvEWItGuwRdgdnLXVcdwLrCdiWPBvGeMJxptUoN3Be7R1zVbgyVkvJrziLDDLFP56X/yFaAE8YyMWv2/8TGZThUWRtJ7qizDDjDBnvbs2l1GQwF4GVT7U2V9mqSYCWQig5XfeAL1J9u/DdvJePWgXvW5FLiFZ7vzQ6XHYDNLalcsJKrCFkQcPHRpSpZ268jKrKkKlpEXs/ORBEQEY7/LCoc2T3jkDjPGtlqxk/pC6eglkFtbK1eH32D2kHHeprcX1OqvKJI7/0ZzL6Uh8zdJ71dCLP5ygq/paHCkN2yWUEd1v9w5YFccHTjg7UDhRdWml3TwN8zUdrWpNliIKgqxcB3C5yAoaJ/C3zWEVjsN6M26PRt9YiUNTkoesVkCtvJVQAJH35tEwHP33DMcbXU= odl_user@cc-4b5f790a-6846b776d8-9xt28"
   }
   os_disk {
     caching           = "ReadWrite"
